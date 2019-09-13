d6a517744b6262ef862b1739b8950f990532d029
fctoibm@gmail.com <fctoibm@gmail.com>
2019-09-13 17:33:22 -0500

    sixth commit

--- roles/create_ocp4_kvms/files/create_kvm.sh
+++ roles/create_ocp4_kvms/files/create_kvm.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in master-{0..2}
+for i in master{0..2}
 do 
   virt-install --name="ocp4-${i}" --vcpus=4 --ram=12288 \
   --disk path="$1"/ocp4-${i}.qcow2,bus=virtio,size=120 \
@@ -9,7 +9,7 @@ do
   virsh define --file ocp4-$i.xml
 done
 
-for i in worker-{0..2} bootstrap
+for i in worker{0..2} bootstrap
 do 
   virt-install --name="ocp4-${i}" --vcpus=4 --ram=8192 \
   --disk path="$1"/ocp4-${i}.qcow2,bus=virtio,size=120 \
